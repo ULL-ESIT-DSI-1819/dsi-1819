6370bdf2fd5237a53824fbeefab61d8d688ef13e
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2020-02-02 08:39:20 +0000

    curso 2019-2020

--- assets/src/search.js
+++ assets/src/search.js
@@ -24,7 +24,7 @@ class JekyllSearch {
   async displayResults() {
     const results = await this.findResults()
     const html = results.map(item => {
-      Console.log(item)
+      console.log(item)
       return `
         <li class="result">
             <article class="result__article  article">
